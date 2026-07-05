1010c10fab2f6f8bfa2941c3c2b3de63e3e77af9
visheshtiwari211 <visheshtiwari211@gmail.com>
2026-07-05 19:22:36 +0530

    Add: subsets-ii - submission-4

---
+++ Data Structures & Algorithms/subsets-ii/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> l1 = new ArrayList();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        getS(nums, 0, new ArrayList());
+        return l1;
+    }
+
+    public void getS(int[] nums, int i, List<Integer> l2) {
+        if(i >= nums.length) {
+            l1.add(new ArrayList(l2));
+            return;
+        }
+        
+        int next = i + 1;
+        while(next < nums.length && nums[next] == nums[i]) next++;
+        getS(nums, next, l2);
+        l2.add(nums[i]);
+        getS(nums, i + 1, l2);
+        l2.removeLast();
+        return; 
+    }
+}

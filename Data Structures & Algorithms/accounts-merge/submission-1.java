ccbc54bccd4bbb9978cf3d8fc29bc3a01e856ed7
visheshtiwari211 <visheshtiwari211@gmail.com>
2026-08-08 21:54:18 +0530

    Add: accounts-merge - submission-1

---
+++ Data Structures & Algorithms/accounts-merge/submission-1.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> acc) {
+        HashMap<String, String> map = new HashMap();
+        HashMap<String, String> emailName = new HashMap();
+        for(int i =0; i<acc.size(); i++) {
+            for(int j = 1; j<acc.get(i).size(); j++) {
+                if(!map.containsKey(acc.get(i).get(j))) {
+                    map.put(acc.get(i).get(j), acc.get(i).get(j));
+                }
+
+                if(!emailName.containsKey(acc.get(i).get(j))) {
+                    emailName.put(acc.get(i).get(j), acc.get(i).get(0));
+                }
+            }
+        }
+
+        for(int i =0; i<acc.size(); i++) {
+            for(int j = 2; j<acc.get(i).size(); j++) {
+                String firstRoot = find(acc.get(i).get(j-1), map);
+                String secondRoot = find(acc.get(i).get(j), map);
+                if(!firstRoot.equals(secondRoot)) {
+                    map.put(firstRoot, secondRoot);
+                }
+            }
+        }
+        HashMap<String, List<String>> m1 = new HashMap();
+        for(String email : map.keySet()) {
+            String root = find(email, map);
+            if(!m1.containsKey(root)) {
+                m1.put(root, new ArrayList<>());
+            }
+            m1.get(root).add(email);
+        }
+
+        List<List<String>> l1 = new ArrayList<>();
+        for(Map.Entry<String, List<String>> entry: m1.entrySet()) {
+            String name = emailName.get(entry.getKey());
+            List<String> emails = entry.getValue();
+            Collections.sort(emails);
+            List<String> f = new ArrayList<>();
+            f.add(name);
+            f.addAll(emails); 
+            l1.add(f);
+        }
+        return l1;
+    }
+
+    public String find(String x, HashMap<String, String> map) {
+        if(x.equals(map.get(x))) return x;
+
+        return find(map.get(x), map);
+    }
+}
